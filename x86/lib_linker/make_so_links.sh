2b0618692fe9f18f287d077d2f09feb444c7bc38
OoMrFlibble <mrflibble@spotty.net>
2025-07-25 00:15:55 +0100

    More dep fixes

--- x86/lib_linker/make_so_links.sh
+++ x86/lib_linker/make_so_links.sh
@@ -1,36 +1,46 @@
 #!/bin/sh
 
-# Flibble tweaked this on 20250619
+# Flibble tweaked this .
 # added stanza to copy required libraries from build system
 # and made symlink references more dynamic
-# Also added 'clean' function.
+#
+# If we are here, then use_deps is in play, so we might like to
+#  bundle anything which might break on other distros.
+
+p=i386
 
 if [ "x$1" = "xclean" ] ; then
   rm *.so
   rm -Rf ../lib
+  rm -Rf ../../espeak-ng-data
   exit
 fi
 
 mkdir -p ../lib
+if [ "x$use_deps" = "x" ] ; then
+  set > ../lib/use_deps.so
+fi
 cp ../lib-static/* ../lib/
 cp --dereference \
 /usr/lib/libgnustep-base.so.[0-9].[0-9][0-9] \
-/usr/lib/i386-linux-gnu/libicudata.so.[0-9][0-9] \
-/usr/lib/i386-linux-gnu/libicui18n.so.[0-9][0-9] \
-/usr/lib/i386-linux-gnu/libicuuc.so.[0-9][0-9] \
+/usr/lib/$p-linux-gnu/libicudata.so.[0-9][0-9] \
+/usr/lib/$p-linux-gnu/libicui18n.so.[0-9][0-9] \
+/usr/lib/$p-linux-gnu/libicuuc.so.[0-9][0-9] \
 ../lib
+
+#cp -rf /usr/lib/$p-linux-gnu/espeak-ng-data ../../
 
 #These work using distro libraries so should only need re-including where
 # a future distro no longer has one of them at an appropriate version.
-#/usr/lib/i386-linux-gnu/libcaca.so.0
-#/usr/lib/i386-linux-gnu/libffi.so.[0-9]
-#/usr/lib/i386-linux-gnu/libnspr[0-9].so
-#/usr/lib/i386-linux-gnu/libobjc.so.[0-9]
-#/usr/lib/i386-linux-gnu/libportaudio.so.[0-9]
-#/usr/lib/i386-linux-gnu/libsndio.so.[0-9].0
-#/usr/lib/i386-linux-gnu/libxslt.so.[0-9]
-#/usr/lib/i386-linux-gnu/libjack.so.[0-9]
-
+#/usr/lib/$p-linux-gnu/libespeak-ng.so.[0-9]
+#/usr/lib/$p-linux-gnu/libcaca.so.0
+#/usr/lib/$p-linux-gnu/libffi.so.[0-9]
+#/usr/lib/$p-linux-gnu/libjack.so.[0-9]
+#/usr/lib/$p-linux-gnu/libportaudio.so.[0-9]
+#/usr/lib/$p-linux-gnu/libsndio.so.[0-9].0
+#/usr/lib/$p-linux-gnu/libnspr[0-9].so
+#/usr/lib/$p-linux-gnu/libobjc.so.[0-9]
+#/usr/lib/$p-linux-gnu/libxslt.so.[0-9]
 
 #essential
 ##if ! [ -f 'libespeak.so' ]; then
